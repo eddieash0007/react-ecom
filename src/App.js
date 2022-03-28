5912ce0e23f7cbcf7aa6ac13e372c310ee901117
Eddie Ash <edwinashie289@outlook.com>
2022-03-28 17:01:03 +0000

    code refactoring and navbar complete

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react'
-import Products from './components/Products/Products.jsx'
+
+
+import {Products, Navbar} from './components'
+
 
 const App = () => {
   return (
-    <div><Products /></div>
+    <div>
+      <Navbar/>
+      <Products />
+    </div>
   )
 }
 
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -2,15 +2,17 @@ import React from "react";
 import {Grid} from "@material-ui/core"
 
 import Product from "./Product/Product";
+import useStyles from './styles'
 const products = [
     {id:1, name:'Shoes', description:'Running shoes', price: '$5', image: 'https://media.self.com/photos/5ea9d5ffbb9c6b75996c7ef7/master/pass/running_shoes_woman.jpg', },
     {id:2, name:'Macbook', description: 'Apple macbook', price: '$10', image: 'https://images.idgesg.net/images/idge/imported/imageapi/2021/10/18/20/14-16-inch-macbook-pro-2021-100907465-large.jpg?auto=webp&quality=85,70'}
 ]
 
 const Products = () => {
-
+    const classes = useStyles()
     return(
-            <main>
+            <main className={classes.content}>
+                <div className={classes.toolbar}/>
                 <Grid container justify="center" spacing={4}>
                     {products.map((product) => (
                         <Grid item key={product.id} xs={12} md={4} lg={3}>
