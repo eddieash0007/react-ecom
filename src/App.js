3b31c9aa1c8acd409b188944b0f3cfa44d087096
Eddie Ash <edwinashie289@outlook.com>
2022-03-30 09:22:12 +0000

    cart total items implemented

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import {Products, Navbar} from './components'
 
 const App = () => {
   const [products, setProducts] = useState([])
+  const [cart, setCart] = useState({})
 
   const fetchProducts = async () => {
     const {data} = await commerce.products.list()
@@ -13,15 +14,25 @@ const App = () => {
     setProducts(data);
   }
 
+  const fetchCart = async () => {
+    setCart(await commerce.cart.retrieve())
+  }
+
+  const handleAddToCart = async (productId, quantity) => {
+    const item = await commerce.cart.add(productId, quantity)
+    setCart(item.cart)
+  }
+
   useEffect(() => {
     fetchProducts();
+    fetchCart()
   }, [])
 
-  
+  console.log(cart)
   return (
     <div>
-      <Navbar/>
-      <Products products={products}/>
+      <Navbar totalItems={cart.total_items}/>
+      <Products products={products} onAddToCart={handleAddToCart}/>
     </div>
   )
 }
